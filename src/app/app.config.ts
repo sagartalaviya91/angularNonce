ebd84735a757e2e90e08b9c4eb2391928e2fd5a1
sagartalaviya94 <68340446+sagartalaviya94@users.noreply.github.com>
2024-07-24 08:25:47 +0530

    app init

---
+++ src/app/app.config.ts
@@ -0,0 +1,21 @@
+import { ApplicationConfig, CSP_NONCE, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+function getNonce(){
+  const metaTag = document.querySelector('meta[name=cspnonce]') as HTMLMetaElement;
+  const cspNonce = metaTag.content;
+  return cspNonce;
+}
+export const appConfig: ApplicationConfig = {
+  providers: [
+    {
+      provide:CSP_NONCE,
+      useValue:getNonce()
+    },
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+  ],
+};
