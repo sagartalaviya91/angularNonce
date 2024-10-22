e64bd3208bb74f58346b1cd5afb7dced620f0430
sagartalaviya91 <41993850+sagartalaviya91@users.noreply.github.com>
2024-10-22 13:00:30 +0530

    Update customScript.js

--- customScript.js
+++ customScript.js
@@ -15,40 +15,59 @@ function processFile(filePath, outputDirectory) {
         }
 
         // Updated regex to match both `control.addValidator` and `control?.addValidator`
-        const addValidatorRegex = /(\w+)\??\.addValidator\((\w+)\("([^"]+)"\)\)/g;
+        const addValidatorRegex = /(\w+)\?.addValidator\((\w+)\(\[?([^)\]]+)\]?\s*,\s*"([^"]+)"\)\)/g;
         let match;
-        let declarations = '';
         let replacements = data;
         let counter = 0;
-        let alreadyDeclared = new Set(); // To track already declared variables
+
+        // Track validator declarations for each function
+        let declarations = {};
+        let functionScopes = [];
 
         // Find each instance of addValidator with custom validators and messages
         while ((match = addValidatorRegex.exec(data)) !== null) {
             const control = match[1];
             const functionName = match[2];
-            const errorMessage = match[3];
+            const validatorValues = match[3];
+            const errorMessage = match[4];
 
-            // Generate a unique variable name
+            // Generate a unique variable name for the validator
             const variableName = generateVariableName(functionName, errorMessage, counter);
 
-            // If this validator+message combination is already declared, don't declare again
-            if (!alreadyDeclared.has(variableName)) {
-                declarations += `const ${variableName} = ${functionName}("${errorMessage}");\n`;
-                alreadyDeclared.add(variableName);
-                counter++;
+            // Find the function this validator is in
+            const currentScope = functionScopes[functionScopes.length - 1] || 'global';
+
+            if (!declarations[currentScope]) {
+                declarations[currentScope] = new Set();
             }
 
-            // Replace the function call with the generated variable
-            const originalCall = `${control}?.addValidator(${functionName}("${errorMessage}"))`;
+            // If this validator+message combination is already declared in the current function, skip
+            if (!declarations[currentScope].has(variableName)) {
+                // Declare the variable at the start of the function
+                const validatorDeclaration = `const ${variableName} = ${functionName}([${validatorValues}], "${errorMessage}");\n`;
+
+                if (declarations[currentScope].size === 0) {
+                    // Insert declaration at the start of the function (or global scope)
+                    const functionStartIndex = replacements.indexOf('{', replacements.lastIndexOf(`function`));
+                    replacements = [
+                        replacements.slice(0, functionStartIndex + 1),
+                        '\n' + validatorDeclaration,
+                        replacements.slice(functionStartIndex + 1),
+                    ].join('');
+                } else {
+                    // Insert subsequent declarations in the correct scope
+                    replacements = validatorDeclaration + replacements;
+                }
+
+                declarations[currentScope].add(variableName);
+            }
+
+            // Replace the original validator call with the variable reference
+            const originalCall = `${control}?.addValidator(${functionName}([${validatorValues}], "${errorMessage}"))`;
             const replacementCall = `${control}?.addValidator(${variableName})`;
-
-            // Replace in the content
             replacements = replacements.replace(originalCall, replacementCall);
-        }
 
-        // Prepend the variable declarations to the top of the file
-        if (declarations) {
-            replacements = declarations + '\n' + replacements;
+            counter++;
         }
 
         // Create the new file in the output directory
@@ -85,9 +104,9 @@ function processDirectory(directoryPath, outputDirectory) {
 }
 
 // Path to the directory where your Angular project files are located
-const directoryPath = './';
+const directoryPath = '/path/to/your/angular/project';
 // Path to the directory where the modified files will be saved
-const outputDirectory = './output';
+const outputDirectory = '/path/to/output/directory';
 
 // Start processing the directory
 processDirectory(directoryPath, outputDirectory);
